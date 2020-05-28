a7a123bb6cef96d94bfdaa119226f1481351bfb1
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-28 18:14:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,1 @@
 require 'pry'
-def reverse_each_word(string)
-   array = string.split(' ')
-   new_array = [] 
-   array.each do |word|
-     new_string = ''
-    
-     counter = word.length - 1 
-     while counter >= 0 
-     new_string += word[counter]
-     counter -- 
-    
-   new_array << new_string
-   
-   end
-   
-end 
-return new_array.join(" ")
-end
