6ff2facfd18fcfab916971df739da107ca425aca
Diego Zegarra <diego.zegarra79@icloud.com>
2020-05-28 18:17:42 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,7 +4,7 @@ def reverse_each_word(string)
   array = string.split(" ")
   new_array = []
   array.collect do |string| 
-    new_array << string.reverse.join(" ") 
+    new_array << string.reverse 
   end 
-  new_array
+  new_array.join(" ")
 end
